55f5670948a4258fe57fd49feee9a21b4ba4e1a1
diogotim <98195230+diogotim@users.noreply.github.com>
2022-02-01 17:29:17 -0300

    fix tests

---
+++ __tests__/best-sellers.test.ts
@@ -0,0 +1,13 @@
+import { build } from "./helper";
+
+const app = build();
+
+test("best-sellers route", async () => {
+  const res = await app.inject({
+    method: "GET",
+    url: "v1/best-sellers",
+  });
+
+  expect(res.statusCode).toEqual(200);
+  expect(res.json().data.length).toEqual(5);
+});
